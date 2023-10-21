ca64a02c3862be58adb48162542443267d546fd1
patrick ishimwe <52107697+pattyish@users.noreply.github.com>
2023-10-21 00:21:07 -0400

    Creating pipelines

--- bookscarper/bookscarper/pipelines.py
+++ bookscarper/bookscarper/pipelines.py
@@ -10,4 +10,56 @@ from itemadapter import ItemAdapter
 
 class BookscarperPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        # Strip all whitespoaces from strings
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value.strip()
+
+        # Category & Product Type into lowercase
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            adapter[lowercase_key] = value.lower()
+
+        # Convert price to float from string
+        price_fields = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
+        for price_field in price_fields:
+            value = adapter.get(price_field)
+            value = value.replace('£', '')
+            adapter[price_field] = float(value)
+
+        # Availability: Extract only number from string
+        availability_string = adapter.get('availability')
+        split_availability = availability_string.split('(')
+        if len(split_availability) < 2:
+            adapter['availability'] = 0
+        else:
+            availability_arr = split_availability[1].split(' ')
+            adapter['availability'] = float(availability_arr[0])
+
+        # Reviews: Convert number of reviews from string to int
+        num_reviews = adapter.get('num_reviews')
+        adapter['num_reviews'] = int(num_reviews)
+
+        # Stars: Convert text to number
+        stars = adapter.get('stars')
+        star_text_value = stars.split(' ')[1]
+        star_text_value = star_text_value.lower()
+        if star_text_value == "zero":
+            adapter['stars'] = 0
+        elif star_text_value == "one":
+            adapter['stars'] = 1
+        elif star_text_value == "two":
+            adapter['stars'] = 2
+        elif star_text_value == "three":
+            adapter['stars'] = 3
+        elif star_text_value == "four":
+            adapter['stars'] = 4
+        elif star_text_value == "five":
+            adapter['stars'] = 5
+
         return item
--- bookscarper/bookscarper/spiders/bookspider.py
+++ bookscarper/bookscarper/spiders/bookspider.py
@@ -1,4 +1,5 @@
 import scrapy
+from bookscarper.items import BookItem
 
 
 class BookspiderSpider(scrapy.Spider):
@@ -27,17 +28,20 @@ class BookspiderSpider(scrapy.Spider):
 
     def parse_book_page(self, response):
         tables_rows = response.css("table tr")
-        yield {
-            'url': response.url,
-            'title': response.css('.product_main h1::text').get(),
-            'product_type': tables_rows[1].css('td ::text').get(),
-            'price_excl_tax': tables_rows[2].css('td ::text').get(),
-            'price_incl_tax': tables_rows[3].css('td ::text').get(),
-            'tax': tables_rows[4].css('td ::text').get(),
-            'availability': tables_rows[5].css('td ::text').get(),
-            'num_reviews': tables_rows[6].css('td ::text').get(),
-            'stars': response.css('p.star-rating').attrib['class'],
-            'category': response.xpath('//ul[@class="breadcrumb"]/li[@class="active"]/preceding-sibling::li[1]/a/text()').get(),
-            'description': response.xpath('//div[@id="product_description"]/following-sibling::p/text()').get(),
-            'price': response.css('p.price_color ::text').get()
-        }
+        book_item = BookItem()
+        book_item['url'] = response.url
+        book_item['title'] = response.css('.product_main h1::text').get()
+        book_item['product_type'] = tables_rows[1].css('td ::text').get()
+        book_item['price_excl_tax'] = tables_rows[2].css('td ::text').get()
+        book_item['price_incl_tax'] = tables_rows[3].css('td ::text').get()
+        book_item['tax'] = tables_rows[4].css('td ::text').get()
+        book_item['availability'] = tables_rows[5].css('td ::text').get()
+        book_item['num_reviews'] = tables_rows[6].css('td ::text').get()
+        book_item['stars'] = response.css('p.star-rating').attrib['class']
+        book_item['category'] = response.xpath(
+            '//ul[@class="breadcrumb"]/li[@class="active"]/preceding-sibling::li[1]/a/text()').get()
+        book_item['description'] = response.xpath(
+            '//div[@id="product_description"]/following-sibling::p/text()').get()
+        book_item['price'] = response.css('p.price_color ::text').get()
+
+        yield book_item
